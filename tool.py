3767a42f2d3f8ddd894d6e326a738f48a4942327
Irad Aharoni <irad.aharoni@alcide.io>
2019-03-03 11:46:50 +0200

    Adding default values

--- tool.py
+++ tool.py
@@ -54,11 +54,7 @@ def load_config_key():
                 cfile.write(key)
         else:
             with open(config, "r") as cfile:
-<<<<<<< HEAD
                 key = cfile.read().splitlines()[0]
-=======
-                key = cfile.read()
->>>>>>> d191d01303b2719c8cf7c169c4ed905d2ae2fc93
         if key:
             api_token = key
         else:
@@ -123,12 +119,7 @@ def list_team_codes():
 # listcodes=False
 # String, API key to use.
 # apikey='901186e638fd4baa999bb2c32f4ef7d5'
-<<<<<<< HEAD
 def run(league='PL', time=6, standings=True, team=None, live=False, use12hour=False, players=False, output_format='stdout', output_file=None, upcoming=False, lookup=False, listcodes=False, apikey=None):
-=======
-def run(league, time, standings, team, live, use12hour, players,
-         output_format, output_file, upcoming, lookup, listcodes, apikey):
->>>>>>> d191d01303b2719c8cf7c169c4ed905d2ae2fc93
     """
     A CLI for live and past football scores from various football leagues.
 
@@ -148,11 +139,8 @@ def run(league, time, standings, team, live, use12hour, players,
     - PD: Primera Division
     - BSA: Brazil Serie A
     """
-<<<<<<< HEAD
     if not apikey:
         apikey = load_config_key()
-=======
->>>>>>> d191d01303b2719c8cf7c169c4ed905d2ae2fc93
     headers = {'X-Auth-Token': apikey}
 
     try:
